8f0ac0bf5cb25c7c442d456de2e397dd2157b2b0
DiegohTheBooks <diegohcavalcantti@gmail.com>
2026-09-24 16:38:24 -0300

    V6 Modular: criar estrutura inicial do projeto

---
+++ js/battle/battle.js
@@ -0,0 +1,10 @@
+export function createBattleState() {
+    return {
+        playerHp: 20,
+        enemyHp: 20,
+        playerMana: 2,
+        enemyMana: 2,
+        round: 1,
+        lanes: 5
+    };
+}
