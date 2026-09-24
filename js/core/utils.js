68f938cf0c31e3fe11fa0764fbdecdc6c8f72c88
DiegohTheBooks <diegohcavalcantti@gmail.com>
2026-09-24 16:38:11 -0300

    V6 Modular: criar estrutura inicial do projeto

---
+++ js/core/utils.js
@@ -0,0 +1,12 @@
+export function escapeHtml(value = "") {
+    return String(value)
+        .replaceAll("&", "&amp;")
+        .replaceAll("<", "&lt;")
+        .replaceAll(">", "&gt;")
+        .replaceAll('"', "&quot;")
+        .replaceAll("'", "&#039;");
+}
+
+export function getCardImage(card) {
+    return card?.image || card?.imageData || card?.art || "";
+}
